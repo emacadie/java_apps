53f5267fdf263f732b5b3f83362a59791f4c3cd4
Eric MacAdie <eric@MacAdie.net>
2020-06-18 01:49:19 -0500

    Added RewardByConversionServiceTest

---
+++ junit5/src/test/java/com/coffee/reward/RewardByConversionServiceTest.java
@@ -0,0 +1,93 @@
+package com.coffee.reward;
+
+import com.coffee.product.Product;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RewardByConversionServiceTest {
+
+    private RewardByConversionService reward = null;
+
+    @BeforeEach // cannot be static
+    void setUp() {
+        System.out.println( "BeforeEach" );
+        reward = new RewardByConversionService();
+        reward.setAmount( 10 );
+        reward.setNeededPoints( 100 );
+    }
+
+    @Test
+    @DisplayName( "Correct amount is set" )
+    void correctAmount() {
+        Assertions.assertEquals( 10, reward.getAmount() );
+    }
+
+    @Test
+    @DisplayName( "Correct points are set" )
+    void correctPoint() {
+        Assertions.assertEquals( 100, reward.getNeededPoints() );
+    }
+
+    @Test
+    @DisplayName( "When empty order and zero points no rewards should be applied" )
+    void emptyOrderZeroPoints() {
+        RewardInformation info = reward.applyReward( this.getEmptyOrder(), 0 );
+        Assertions.assertEquals( 0, info.getDiscount() );
+        Assertions.assertEquals( 0, info.getPointsRedeemed() );
+    }
+
+    @Test
+    @DisplayName( "When not enough points no rewards should be applied" )
+    void notEnoughPoints() {
+        RewardInformation info = reward.applyReward( this.getSampleOrder(), 0 );
+        Assertions.assertEquals( 0, info.getDiscount() );
+        Assertions.assertEquals( 0, info.getPointsRedeemed() );
+    }
+
+    @Test
+    @DisplayName( "When empty order and enough points no rewards should be applied" )
+    void emptyOrderEnoughPoints() {
+        RewardInformation info = reward.applyReward( this.getEmptyOrder(), 200 );
+        Assertions.assertEquals( 0, info.getDiscount() );
+        Assertions.assertEquals( 0, info.getPointsRedeemed() );
+    }
+
+    @Test
+    @DisplayName( "When empty order and enough points no rewards should be applied w/assumption" )
+    void emptyOrderEnoughPointsWithAssumption() {
+        RewardInformation info = reward.applyReward( this.getEmptyOrder(), 200 );
+        Assertions.assertEquals( 0, info.getDiscount() );
+        Assumptions.assumeTrue( "1".equals( System.getenv( "TEST_POINTS" ) ) );
+        Assertions.assertEquals( 0, info.getPointsRedeemed() );
+    }
+
+    @Test
+    @DisplayName( "When empty order and enough points no rewards should be applied w/AssumingThat" )
+    void emptyOrderEnoughPointsWithAssumingThat() {
+        RewardInformation info = reward.applyReward( this.getEmptyOrder(), 200 );
+        Assertions.assertEquals( 0, info.getDiscount() );
+        Assumptions.assumingThat( !"1".equals( System.getenv( "TEST_POINTS" ) ) ,
+                () -> {
+                    Assertions.assertEquals( 0, info.getPointsRedeemed() );
+                });
+
+    }
+
+    private List< Product > getEmptyOrder() {
+        return Arrays.asList();
+    }
+
+    private List< Product > getSampleOrder() {
+        Product bigDecaf = new Product( 2, "Big Decaf", 2.49 );
+        Product bigLatte = new Product( 3, "Big Latte", 2.99 );
+        Product bigTea   = new Product( 4, "Big Tea", 2.99 );
+        Product espresso = new Product( 5, "Espresso", 2.99 );
+        return Arrays.asList( bigDecaf, bigLatte, bigTea, espresso );
+    }
+}
