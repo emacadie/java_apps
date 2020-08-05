ef7864c6237babaee598692e37fbbd6a2ebccb68
Eric MacAdie <eric@MacAdie.net>
2020-08-04 21:22:10 -0500

    Adding an AutoConfiguration

--- spring.boot/just-gif-it/src/main/java/info/shelfunit/justgifit/controllers/UploadController.java
+++ spring.boot/just-gif-it/src/main/java/info/shelfunit/justgifit/controllers/UploadController.java
@@ -1,5 +1,11 @@
 package info.shelfunit.justgifit.controllers;
 
+import info.shelfunit.justgifit.services.ConverterService;
+import info.shelfunit.justgifit.services.GifEncoderService;
+import info.shelfunit.justgifit.services.VideoDecoderService;
+import com.madgag.gif.fmsware.AnimatedGifEncoder;
+import org.bytedeco.javacv.FFmpegFrameGrabber;
+import org.bytedeco.javacv.FrameGrabber;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,9 +17,12 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
 import java.lang.invoke.MethodHandles;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 public class UploadController {
@@ -23,16 +32,34 @@ public class UploadController {
     @Value( "${multipart.location}" )
     private String location;
 
+    @Inject
+    private ConverterService converterService;
+
+    @Inject
+    private GifEncoderService gifEncoderService;
+
+    @Inject
+    private VideoDecoderService videoDecoderService;
+
     @RequestMapping( value = "/upload", method = RequestMethod.POST, produces = MediaType.IMAGE_GIF_VALUE )
     public String upload( @RequestPart( "file" )    MultipartFile file,
                           @RequestParam( "start" )  int start,
                           @RequestParam( "end" )    int end,
                           @RequestParam( "speed" )  int speed,
-                          @RequestParam( "repeat" ) boolean repeat ) throws IOException {
+                          @RequestParam( "repeat" ) boolean repeat ) throws IOException, FrameGrabber.Exception  {
         File videoFile = new File( location + "/" + System.currentTimeMillis() + ".mp4" );
         file.transferTo( videoFile );
         log.info( "Saved file to {}", videoFile.getAbsolutePath() );
 
-        return "";
+        Path output = Paths.get(location + "/gif/" + System.currentTimeMillis() + ".gif");
+
+        FFmpegFrameGrabber frameGrabber = videoDecoderService.read( videoFile );
+        AnimatedGifEncoder gifEncoder = gifEncoderService.getGifEncoder( repeat,
+                ( float ) frameGrabber.getFrameRate(), output );
+        converterService.toAnimatedGif( frameGrabber, gifEncoder, start, end, speed );
+
+        log.info( "Saved generated gif to {}", output.toString() );
+
+        return output.getFileName().toString();
     }
 }
